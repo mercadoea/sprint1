40e7c1da263da62d7feaf2c33dd23c8c2c9ef8e3
albe <alberobay181@gmail.com>
2022-08-19 08:23:36 -0500

    se agrego texto

--- src/main/principal.java
+++ src/main/principal.java
@@ -3,6 +3,6 @@ package main;
 public class principal {
     public static void main(String[] args) {
         System.out.print("aqui empezamos");
-        System.out.print("hagamos commits");
+        System.out.print("hagamos comentarios");
     }
 }
